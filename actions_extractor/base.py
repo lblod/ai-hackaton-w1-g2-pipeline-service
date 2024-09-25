5f1ca26b47d6fbf8461a5b899d2688f0eb5b3857
Jarne Demeulemeester <jarnedemeulemeester@gmail.com>
2024-09-25 13:50:12 +0200

    feat: improve llm output and add categories

--- actions_extractor/base.py
+++ actions_extractor/base.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
 
-from actions_extractor.models import Actions
+from actions_extractor.models import ActionFragment
 
 
 class ActionsExtractor(ABC):
     @abstractmethod
-    def extract_actions(self, text: str) -> Actions:
+    def extract_actions(self, text: str) -> list[ActionFragment]:
         pass
--- actions_extractor/models.py
+++ actions_extractor/models.py
@@ -1,18 +1,40 @@
-from typing import List, Optional
+from enum import StrEnum
+from typing import Optional
 
-from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
+
+
+class ActionCategory(StrEnum):
+    DAK_BUITENMUUR_DAKBEDEKKING_GOOT = "DAK_BUITENMUUR_DAKBEDEKKING_GOOT"
+    DAK_BUITENMUUR_AFWERKINGSLAAG_KLEUR = "DAK_BUITENMUUR_AFWERKINGSLAAG_KLEUR"
+    DAK_BUITENMUUR_AFWERKINGSLAAG_TEXTUUR = "DAK_BUITENMUUR_AFWERKINGSLAAG_TEXTUUR"
+    DAK_BUITENMUUR_BUITENSCHRIJNWERK = "DAK_BUITENMUUR_BUITENSCHRIJNWERK"
+    DAK_BUITENMUUR_VASTE_ELEMENTEN = "DAK_BUITENMUUR_VASTE_ELEMENTEN"
+    TUIN_PARK_BEGRAAFPLAATS = "TUIN_PARK_BEGRAAFPLAATS"
+    INTERIEUR_ARCHITECTUUR = "INTERIEUR_ARCHITECTUUR"
+    INTERIEUR_DECORATIE = "INTERIEUR_DECORATIE"
+
+
+class MeasureType(StrEnum):
+    NEEDS_PERMIT = "NeedsPermit"
+    FORBIDDEN = "Forbidden"
+    ALLOWED_WITHOUT_PERMIT = "AllowedWithoutPermit"
 
 
 class Action(BaseModel):
     """"Information about the extracted action"""
     action: Optional[str] = Field(description="The complete text of an action")
+    forbidden: Optional[bool] = Field(description="Is the action forbidden on the heritage property", default=False)
     permit_needed: Optional[bool] = Field(description="Is a permit needed for this action", default=True)
+    category: Optional[str] = Field(description="Category the action belongs to")
 
 
 class Actions(BaseModel):
-    allowed_action_list: Optional[List[Action]]
-    not_allowed_action_list: Optional[List[Action]]
+    allowed_action_list: list[Action] = []
+    not_allowed_action_list: list[Action] = []
 
 
-output_parser= PydanticOutputParser(pydantic_object=Actions)
+class ActionFragment(BaseModel):
+    text_fragment: str
+    measure_type: MeasureType
+    category: ActionCategory | None
